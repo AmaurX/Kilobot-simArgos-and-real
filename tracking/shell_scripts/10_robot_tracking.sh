591af8dfeb91714833835867091ba922969c20ef
AmaurX <amaury.camus92@gmail.com>
2018-06-20 11:19:13 +0200

    renew new path in scripts

--- tracking/shell_scripts/10_robot_tracking.sh
+++ tracking/shell_scripts/10_robot_tracking.sh
@@ -3,6 +3,6 @@ export WORKON_HOME=/root/.virtualenvs
 VIRTUALENVWRAPPER_PYTHON='/usr/bin/python'
 . `which virtualenvwrapper.sh`
 workon cv
-for filename in videos/10_robots/*.MP4; do
-    python tracking_scripts/kilobot_tracking.py -v $filename -n 10 -i 0
+for filename in real_experiments/videos/10_robots/*.MP4; do
+    python tracking/tracking_scripts/kilobot_tracking.py -v $filename -n 10 -i 0
 done
--- tracking/shell_scripts/20_robot_tracking.sh
+++ tracking/shell_scripts/20_robot_tracking.sh
@@ -3,6 +3,6 @@ export WORKON_HOME=/root/.virtualenvs
 VIRTUALENVWRAPPER_PYTHON='/usr/bin/python'
 . `which virtualenvwrapper.sh`
 workon cv
-for filename in videos/20_robots/*.MP4; do
-    python tracking_scripts/kilobot_tracking.py -v $filename -n 20 -i 0
+for filename in real_experiments/videos/20_robots/*.MP4; do
+    python tracking/tracking_scripts/kilobot_tracking.py -v $filename -n 20 -i 0
 done
